03058317d8c3a51885de380cca00740bf30a5182
roopxx <127219908+roopxx@users.noreply.github.com>
2024-09-25 18:05:19 +0530

    Add session storage for num_of_visits

--- catalog/views.py
+++ catalog/views.py
@@ -21,13 +21,19 @@ def index(request):
     # Books with word 'The'
     books_with_word = Book.objects.filter(title__icontains = 'the').count()
 
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+    
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
         'num_genres': num_genres,
-        'books_with_word': books_with_word
+        'books_with_word': books_with_word,
+        'num_visits': num_visits
     }
 
     # Render the HTML template index.html with the data in the context variable
